f2d54248386381426a8a44d0f3e31fca78baa9e4
Aline Shayonara <shayramos@hotmail.com>
2018-01-18 13:31:54 -0300

    criação do gráfico

--- Assets/Grafico.cs
+++ Assets/Grafico.cs
@@ -4,33 +4,25 @@ using UnityEngine;
 
 public class Grafico : MonoBehaviour {
 
-	//public GameObject enemy;
-	public GameObject p1, p2, p3, p4, p5;
-	int positionX, positionY;
+	public GameObject linhap1, linhap2, linhap3, linhap4, linhap5;
+    static int index = 0;
+    // Use this for initialization
+    void Start()
+    {
+     //   Invoke("executando", 2);
+        InvokeRepeating("executando", 1, 1);
+    }
 
-	// Use this for initialization
-	void Start () {
-		GameObject quad = GameObject.CreatePrimitive (PrimitiveType.Quad);
-		GameObject barra = new GameObject ();
-		quad.transform.parent = barra.transform;
-		barra.name = "Barra";
-		barra.transform.parent = GameObject.Find ("Grafico").transform;
-		barra.transform.localPosition = new Vector3 (-100, 80);
-		barra.transform.localScale = new Vector3 (50, 30);
-
-	//p1.transform.SetAsLastSibling ();
-	//	for (int i = 0; i < 5; i++) {
-	//		Instantiate(enemy);
-	//	}
-	}
-
-	void OnGUI()
-	{
-		GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
-
-	}
+    public void executando()
+    {
+        if (index < 13)
+        {
+            GameObject tempo1 = linhap1.transform.GetChild(index++).gameObject;
+            tempo1.SetActive(true);
+        }
+    }
 	
-	// Update is called once per frame
+    // Update is called once per frame
 	void Update () {
 		
 	}
