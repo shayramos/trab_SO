3a5e80e1df9001bc210a718703d8eef745a1c8e9
Aline Shayonara <shayramos@hotmail.com>
2018-01-21 01:00:06 -0300

    Refazendo gráfico

--- Assets/Grafico.cs
+++ Assets/Grafico.cs
@@ -12,174 +12,96 @@ namespace AssemblyCSharp
         static int index = 0;
         public main script_main;
         Processo processo;
+        GameObject line;
 
         // Use this for initialization
         void Start()
         {
         }
 
-        public int tempoChegada() {
+        public int TempoChegada() {
             int tempoC = this.processo.tempoChegada;
             return tempoC;
         }
 
-
-        public void ExecutarGrafico()
+        public int TempoExecucao()
         {
-			script_main.escalonador.update ();
+            int tempoE = this.processo.tempoExecucao;
+            return tempoE;
+        }
+
+        public GameObject SaberLinha()
+        {
             if (this.processo.idProcesso == 0)
-            {
-                if (script_main.escalonador.algoritmo.Preemptivo)
-                {
-
-                }
-                else
-                {
-                    if (tempoChegada() < index)
-                    {
-                        for (int i = 0; i < this.processo.tempoExecucao; i++)
-                        {
-                            GameObject tempo1 = linhap1.transform.GetChild(index).gameObject;
-                            tempo1.SetActive(true);
-                            index++;
-                        }
-                    }
-                    else
-                    {
-                        for (int i = 0; i < this.processo.tempoExecucao; i++)
-                        {
-                            GameObject tempo1 = linhap1.transform.GetChild(tempoChegada()).gameObject;
-                            tempo1.SetActive(true);
-                            index++;
-                        }
-                    }
-                }
-            }
+                line = this.linhap1;
             else if (this.processo.idProcesso == 1)
-            {
-                if (script_main.escalonador.algoritmo.Preemptivo)
-                {
-
-                }
-                else
-                {
-                    if (tempoChegada() < index)
-                    {
-                        for (int i = 0; i < this.processo.tempoExecucao; i++)
-                        {
-                            GameObject tempo1 = linhap2.transform.GetChild(index).gameObject;
-                            tempo1.SetActive(true);
-                            index++;
-                        }
-                    }
-                    else
-                    {
-                        for (int i = 0; i < this.processo.tempoExecucao; i++)
-                        {
-                            GameObject tempo1 = linhap2.transform.GetChild(tempoChegada()).gameObject;
-                            tempo1.SetActive(true);
-                            index++;
-                        }
-                    }
-                }
-            }
+                line = this.linhap2;
             else if (this.processo.idProcesso == 2)
-            {
-                if (script_main.escalonador.algoritmo.Preemptivo)
-                {
-
-                }
-                {
-                    if (tempoChegada() < index)
-                    {
-                        for (int i = 0; i < this.processo.tempoExecucao; i++)
-                        {
-                            GameObject tempo1 = linhap3.transform.GetChild(index).gameObject;
-                            tempo1.SetActive(true);
-                            index++;
-                        }
-                    }
-                    else
-                    {
-                        for (int i = 0; i < this.processo.tempoExecucao; i++)
-                        {
-                            GameObject tempo1 = linhap3.transform.GetChild(tempoChegada()).gameObject;
-                            tempo1.SetActive(true);
-                            index++;
-                        }
-                    }
-                }
-            }
+                line = this.linhap3;
             else if (this.processo.idProcesso == 3)
-            {
-                if (script_main.escalonador.algoritmo.Preemptivo)
-                {
-
-                }
-                {
-                    if (tempoChegada() < index)
-                    {
-                        for (int i = 0; i < this.processo.tempoExecucao; i++)
-                        {
-                            GameObject tempo1 = linhap4.transform.GetChild(index).gameObject;
-                            tempo1.SetActive(true);
-                            index++;
-                        }
-                    }
-                    else
-                    {
-                        for (int i = 0; i < this.processo.tempoExecucao; i++)
-                        {
-                            GameObject tempo1 = linhap4.transform.GetChild(tempoChegada()).gameObject;
-                            tempo1.SetActive(true);
-                            index++;
-                        }
-                    }
-                }
-            }
+                line = this.linhap4;
             else if (this.processo.idProcesso == 4)
-            {
-                if (script_main.escalonador.algoritmo.Preemptivo)
-                {
+                line = this.linhap5;
+            return line;
+        }
 
-                }
-                {
-                    if (tempoChegada() < index)
-                    {
-                        for (int i = 0; i < this.processo.tempoExecucao; i++)
-                        {
-                            GameObject tempo1 = linhap5.transform.GetChild(index).gameObject;
-                            tempo1.SetActive(true);
-                            index++;
-                        }
-                    }
-                    else
-                    {
-                        for (int i = 0; i < this.processo.tempoExecucao; i++)
-                        {
-                            GameObject tempo1 = linhap5.transform.GetChild(tempoChegada()).gameObject;
-                            tempo1.SetActive(true);
-                            index++;
-                        }
-                    }
-                }
+        public void AtivaBarra(GameObject linha, int index)
+        {
+            GameObject tempo = linha.transform.GetChild(index).gameObject;
+            tempo.SetActive(true);
+        }
+        public void GraficoPreemptivo()
+        {
+            script_main.escalonador.update();
+
+        }
+        public int LocalEntradaProcesso()
+        {
+            int tempo;
+            if (TempoChegada() < index)
+            {
+                tempo = index;
+            }
+            else
+            {
+                tempo = TempoChegada();
+            }
+            return tempo;
+        }
+        public void GraficoSemPreempcao()
+        {
+            while (processo.tempoExecutado < TempoExecucao())
+            {
+                script_main.escalonador.update();
+                AtivaBarra(SaberLinha(), LocalEntradaProcesso());
+                index++;
             }
         }
+       
         public void Comeca()
         {
 			if (script_main.escalonador.Executando == null) {
 				script_main.escalonador.inicializa ();
-			}
-
-			InvokeRepeating ("ExecutarGrafico", 1, 1);
-
+            }
+            //if (!(script_main.escalonador.algoritmo.executar(script_main.escalonador))) {
+            if (script_main.escalonador.algoritmo.Preemptivo)
+                {
+                    InvokeRepeating("GraficoPreemptivo", 1, 1);
+                }
+                else
+                {
+                    //InvokeRepeating("GraficoSemPreempcao", 1, 1);
+                    Invoke("GraficoSemPreempcao", 1);
+                }
+            //}
+			
         }
 
 
         // Update is called once per frame
         void Update()
         {
-
+            this.processo = script_main.escalonador.executando;
         }
 			
     }
